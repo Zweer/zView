ec939e928451602f1c6ded7ba9029344eaad4d70
Niccolò Olivieri <flicofloc@gmail.com>
2013-10-13 16:47:46 +0200

    This is a "singleton-like" plugin, fixed

--- src/core.js
+++ src/core.js
@@ -1,27 +1,28 @@
-	var ZView = function (elements, options) {
-
+	var ZView = function (element, options) {
+		this.element = element;
+		this.options = options;
 	};
 
 	ZView.DEFAULT = {
 
 	};
 
-	$.zView = function (elements, options) {
-		return new ZView(elements, $.extend(true, {}, ZView.DEFAULT, options));
+	$.zView = function (element, options) {
+		return new ZView(element, $.extend(true, {}, ZView.DEFAULT, options));
 	};
 
 	$.fn.zView = function (option) {
-		var $this   = $(this);
-    var data    = $this.data('zview');
-    var options = $.extend({}, $this.data(), typeof option === 'object' && option);
+		return this.each(function () {
+			var $this   = $(this);
+	    var data    = $this.data('zview');
+	    var options = $.extend({}, $this.data(), typeof option === 'object' && option);
 
-    if (!data) {
-      $this.data('zview', (data = $.zView(this, options)));
-    }
+	    if (!data) {
+	      $this.data('zview', (data = $.zView(this, options)));
+	    }
 
-    if (typeof option === 'string') {
-      data[option]($this);
-    }
-
-    return this;
+	    if (typeof option === 'string') {
+	      data[option]($this);
+	    }
+		});
 	};
