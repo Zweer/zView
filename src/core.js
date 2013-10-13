cc878e71a19c96c69c6a9dbe39d29440b27ac7da
Niccolò Olivieri <flicofloc@gmail.com>
2013-10-13 16:34:45 +0200

    Added the jquery code #3

---
+++ src/core.js
@@ -0,0 +1,23 @@
+	var ZView = function (elements, options) {
+
+	};
+
+	$.zView = function (elements, options) {
+		return new ZView(elements, options);
+	};
+
+	$.fn.zView = function (option) {
+		var $this   = $(this);
+    var data    = $this.data('zview');
+    var options = $.extend({}, $this.data(), typeof option === 'object' && option);
+
+    if (!data) {
+      $this.data('zview', (data = $.zView(this, options)));
+    }
+
+    if (typeof option === 'string') {
+      data[option]($this);
+    }
+
+    return this;
+	};
