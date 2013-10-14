91f9b52e4d126a9d6a0f63005277f55682fa1f6f
Niccolo' Olivieri <flicofloc@gmail.com>
2013-10-14 14:03:31 +0200

    Completed the setup phase

--- src/core.js
+++ src/core.js
@@ -1,10 +1,57 @@
   var ZView = function (element, options) {
-    this.element = element;
+    this.$element = $(element);
     this.options = options;
+
+    this._initElement();
+    this._initEvents();
+  };
+
+  ZView.prototype._initElement = function() {
+    this.$element.css({
+      position: this.$element.css('position') !== 'static' ? this.$element.css('position') : 'relative'
+    });
+
+    this.$element.children().each($.proxy(this._initChild, this));
+
+    this.$contents = this.$element.children().css({
+      display: 'none',
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      zIndex: this.options.zIndex
+    });
+  };
+
+  ZView.prototype._initChild = function(index, element) {
+      var $element = $(element);
+
+      switch ($element.prop('tagName').toLowerCase()) {
+        case 'div':
+          return;
+
+        case 'img':
+          $element
+            .wrap($('<div></div>')
+              .css({
+                backgroundImage: 'url("' + $element.attr('src') + '")',
+                backgroundSize: 'cover'
+              })
+            )
+            .remove();
+          break;
+
+        default:
+          $element
+            .wrap($('<div></div>'));
+      }
+  };
+
+  ZView.prototype._initEvents = function() {
+    
   };
 
   ZView.DEFAULT = {
-
+    zIndex: 1
   };
 
   $.zView = function (element, options) {
@@ -18,7 +65,7 @@
       var options = $.extend({}, $this.data(), typeof option === 'object' && option);
 
       if (!data) {
-        $this.data('zview', (data = $.zView(this, options)));
+        $this.data('zview', (data = $.zView($this, options)));
       }
 
       if (typeof option === 'string') {
